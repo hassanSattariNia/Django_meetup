e38511e76c504b359df535ed455c5e237c506356
Hassan Sattari nia <hassansattarinia@gmail.com>
2025-11-23 22:28:08 +0330

    finish item-details page

--- meetups/urls.py
+++ meetups/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     # url , then function have to called when this url witted on the browser
-    path('meetups/',views.index),
-    path('meetups/<slug:meetup_slug>',views.meetup_details)#using dynamic path segment
+    path('meetups/',views.index,name='all-meetups'),
+    path('meetups/<slug:meetup_slug>/', views.meetup_details, name='meetup-details') #using dynamic path segment
 ]
