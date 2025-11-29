ea2be19daa799126f5d65cc32f2ebd8212244037
Hassan Sattari nia <hassansattarinia@gmail.com>
2025-11-29 13:04:25 +0330

    added email to meetup organizer

--- meetups/urls.py
+++ meetups/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     # url , then function have to called when this url witted on the browser
     path('meetups/',views.index,name='all-meetups'),
-    path('meetups/<slug:meetup_slug>/', views.meetup_details, name='meetup-details') #using dynamic path segment
+    path('meetups/success',views.confirm_registeration,name='confirm-registeration'),
+    path('meetups/<slug:meetup_slug>/', views.meetup_details, name='meetup-details'), #using dynamic path segment
+    
 ]
